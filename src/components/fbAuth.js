f720f8a3217bd8b43bd8da297e470bcdd1bbf900
Hamza <hamza.husein017@gmail.com>
2021-04-29 22:25:31 +0530

    refactor firebase keys implementation

--- src/components/fbAuth.js
+++ src/components/fbAuth.js
@@ -3,13 +3,13 @@ import "firebase/firestore"
 import "firebase/auth"
 
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyALnQpF5YfJnVoGSSaCRLlPpuvxEptJuH8",
+  authDomain: "to-do-list-ff102.firebaseapp.com",
+  projectId: "to-do-list-ff102",
+  storageBucket: "to-do-list-ff102.appspot.com",
+  messagingSenderId: "41070404754",
+  appId: "1:41070404754:web:561208489a7ce3a09bf24b",
+  measurementId: "G-YPV9MJDGCM",
 });
 
 const db = firebase.firestore();
